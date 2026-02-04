f0588700cf17a0e68135d287b4d12a0c36828ae8
CNikhitha <chitturilakshminikhitha@gmail.com>
2026-02-04 12:19:13 +0530

    Final app.py

--- app.py
+++ app.py
@@ -68,8 +68,8 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
-app.config['MAIL_USERNAME'] = 'voolapallisarvani@gmail.com'
-app.config['MAIL_PASSWORD'] = 'agubhwwzmwedydoa'  # Gmail app password
+app.config['MAIL_USERNAME'] = 'your-mail@gmail.com'
+app.config['MAIL_PASSWORD'] = '16-digit password'  # Gmail app password
 app.config['MAIL_DEFAULT_SENDER'] = app.config['MAIL_USERNAME']
 
 # Optional SMS configuration (Twilio REST API via stdlib, no new deps)
